366bcd26b7eb60ff5c8cdcf80c87aa53d66d9e29
Tosini13 <jbartos13@gmail.com>
2022-01-31 22:57:11 +0100

    fixed mongo connection and modified package.json automatically

--- server/src/mongo/config.ts
+++ server/src/mongo/config.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectMongoose = () => {
   mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@medline.arwq9.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.1p0oo.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
   ).then(() => {
     console.log("Successfully connected to database");
   })
